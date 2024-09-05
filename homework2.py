9862980b15ea5f9b94161ed33e6c4da5f9bc0ecf
Georgii0900 <yuko0900@mail.ru>
2024-09-05 16:29:36 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+homeworks = 12
+time_spent = 1.5
+course_name = 'Python'
+time_on_one_task = time_spent / homeworks
+print('Курс:', course_name, end=', ')
+print('всего задач:', homeworks, end=', ')
+print('затрачено часов:', time_spent, end=', ')
+print('среднее время выполнения', time_on_one_task, 'часа.' )
